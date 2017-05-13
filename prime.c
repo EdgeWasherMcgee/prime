09bb01d0c72550d834601dd038395062cde8c21b
EdgeWasherMcgee <axel.ingvast@gmail.com>
2017-05-13 09:53:11 +0200

    Only one bug left!

--- prime.c
+++ prime.c
@@ -1,40 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int remove(int tally[], int len, int serie){
+int delserie(int tally[], int len, int serie){
 	for (int x = serie * 2; x < len; x += serie){
 		tally[x] = 0;
 	}
 	return 0;
 }
-int next_prime(tally[], int y){
-	int to! = 1;
-	for ( int x = y; to!; x++){
-		if (tally[x]){
-			return tally[x];
-		}
-	}
-}	
 
+int next_prime(int tally[], int y){
+	while(!tally[y]){
+		y++;
+	}
+	return y;
+}	
 int prime(int len){
 	int primes[len];
 	int list_len = len * len;
 	int tally[list_len];
+	printf("Done\n");
 	for (int x = 0; x < list_len; x++){
 		tally[x] = x;
 	}
+	printf("Done!\n");
 	//
-	for ( int y = 2; while y < len; y = next_prime(tally[], y) ){
-		remove(tally[], list_len, y);
-	}
-	for (int e = 0,int z = 0; z != len; z++){
+	int z = 1;
+	for ( int y = 2; z < len; z++ ) {
+		if (z == len){
+			break;
+		}
+		if (y == 2){
+			delserie(tally, list_len, y);
+		}
+		y = next_prime(tally, y + 1);
+		printf("%d, ", y);
+		if (z % 20 == 0){
+			printf("\n");
+		}
+		delserie(tally, list_len, y);
+	} 
+	printf("\n");
+	for (int z,e = 0; z != len; z++){
 		if (tally[z]){
-			primes[e] = tally[z]
-			e++
+			primes[e] = tally[z];
+			e++;
 		}
 	}
+	//printf("%d\n", primes[4]);
+	//for ( int u = 1; u < len; u++){
+	//	printf("%d,", primes[u]);
+	//}
+	printf("\n");
 	return 0;
 }
 
-int main(){
-	prime(5);
+int main(int argn, char * argv[]){
+	int n = strtod(argv[1], 0);
+	prime(n);
+	return 0;
 }
